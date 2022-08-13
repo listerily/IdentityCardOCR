0f40a0894b26b61de6ab4db5d13b27124008bff8
listerily <listerily@outlook.com>
2022-08-13 14:11:03 +0800

    made cropper adaptable

--- cropper/cropper.py
+++ cropper/cropper.py
@@ -4,13 +4,49 @@ import cv2
 from PIL import Image
 
 
+def automatic_brightness_and_contrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    hist_size = len(hist)
+
+    # Calculate cumulative distribution from the histogram
+    accumulator = [float(hist[0])]
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index - 1] + float(hist[index]))
+
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum / 100.0)
+    clip_hist_percent /= 2.0
+
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+
+    # Locate right cut
+    maximum_gray = hist_size - 1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return auto_result, alpha, beta
+
+
 def read_and_preprocess(filename):
     image = cv2.imread(filename)
+    image, alpha, beta = automatic_brightness_and_contrast(image)
+    image = np.clip((1.7 * image + 70), 0, 255).astype(np.uint8)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    alpha = 1.99
-    beta = 50
-    image = np.clip((alpha * image + beta), 0, 255).astype(np.uint8)
     image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    image = cv2.erode(image, np.ones((4, 4), np.uint8), iterations=1)
+    image = cv2.dilate(image, np.ones((2, 2), np.uint8), iterations=1)
     image = cv2.resize(image, (1792, 1128))
     return image / 255.
 
@@ -40,4 +76,7 @@ def crop(image, show=True, write=False, write_directory='.'):
 
 
 if __name__ == '__main__':
-    crop(read_and_preprocess('./cards/0.png'), show=False, write=True)
+    crop(read_and_preprocess('/home/listerily/test.jpg'), show=True, write=True)
+    crop(read_and_preprocess('/home/listerily/test3.png'), show=True, write=True)
+    crop(read_and_preprocess('/home/listerily/test4.png'), show=True, write=True)
+    crop(read_and_preprocess('/home/listerily/test5.png'), show=True, write=True)

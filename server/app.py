5027a88caccf0f00e6154826ee29f7d9b764f5f0
SYX2021 <87289377+SYX2021@users.noreply.github.com>
2022-08-17 20:21:23 +0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
+import json
+
 import cv2
+import flask
 import numpy as np
 from flask import Flask, request
 from driver.driver import Driver
@@ -19,12 +22,21 @@ def cors(environ):
 def index():
     image = None
     if request.method == 'POST':
-        img_str = request.form['image']
-        img_byte = base64.b64decode(img_str)
-        image = np.fromstring(img_byte, np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # data = json.loads(flask.request.get_data("data"))
+        # data_64 = str.encode(data['data'])
+        img_upload = request.files.get("img_upload")
+        # 判断是否接收到图片
+        if img_upload:
+            # 读取图片
+            image_string = base64.b64encode(img_upload.read())
+            image_string = str(image_string, "utf8")
+        # img_str = request.data['image']
+        # img_byte = base64.b64decode(img_str)
+        # image = np.fromstring(img_byte, np.uint8)
+        # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         #print(f)
         # f.save('/var/www/uploads/uploaded_img.jpg')
+        print("上传")
         return '上传成功'
 
     if request.method == 'GET':
@@ -35,11 +47,13 @@ def index():
         #     'address': 'image_address',
         #     'number': 'image_number'
         # }
-
+        print("获取")
         # 显示结果页面
         return results
+    print("hello")
+    return None
 
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(host="localhost",port=8080)

26a4a05a4397be063142297a806ef41a07c1b279
listerily <listerily@outlook.com>
2022-08-20 21:20:21 +0800

    use button instead of scanning

--- app/app/src/main/java/com/example/identity_card_ocr/CaptureActivity.java
+++ app/app/src/main/java/com/example/identity_card_ocr/CaptureActivity.java
@@ -26,6 +26,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.identity_card_ocr.databinding.ActivityCaptureBinding;
@@ -68,7 +69,7 @@ public class CaptureActivity extends AppCompatActivity {
         int frameWidth = binding.frameLayoutCamera.getWidth();
         int frameHeight = (int) Math.round(frameWidth * 0.63084);
         binding.frameLayoutCamera.setMinimumHeight(frameHeight);
-
+        binding.captureButton.setOnClickListener(view -> captureAndRecognize());
         requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
     }
 
@@ -111,12 +112,6 @@ public class CaptureActivity extends AppCompatActivity {
                     CameraSelector selector = CameraSelector.DEFAULT_BACK_CAMERA;
                     cameraProvider.unbindAll();
                     cameraProvider.bindToLifecycle(CaptureActivity.this, selector, imageCapture, preview);
-                    CaptureActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            capturingThread.start();
-                        }
-                    });
                 } catch (ExecutionException | InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -124,143 +119,109 @@ public class CaptureActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(this));
     }
 
-    @Override
-    protected void onDestroy() {
-        capturingThread.interrupt();
-        super.onDestroy();
-    }
+    public final MediaType mediaType = MediaType.parse("application/text");
+    public static final String postUrl = "http://39.106.132.24:8080/api";
 
-    private boolean analyzing = false;
-    private final Thread capturingThread = new Thread() {
-        public final MediaType mediaType = MediaType.parse("application/text");
-        public static final String postUrl = "http://39.106.132.24:8080/api";
+    private void captureAndRecognize() {
+        File picturePath = new File(CaptureActivity.this.getFilesDir(), "picture.jpeg");
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(picturePath).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(CaptureActivity.this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        String encodedImage;
+                        try {
+                            BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(picturePath));
+                            byte[] bytes = new byte[inputStream.available()];
+                            inputStream.read(bytes, 0, bytes.length);
+                            inputStream.close();
+                            encodedImage = Base64.encodeToString(bytes, Base64.DEFAULT);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
+                                    R.string.capture_failed,
+                                    Toast.LENGTH_LONG).show());
+                            return;
+                        }
+                        OkHttpClient client = new OkHttpClient.Builder()
+                                .connectTimeout(10, TimeUnit.SECONDS)
+                                .writeTimeout(10, TimeUnit.SECONDS)
+                                .readTimeout(30, TimeUnit.SECONDS).build();
+                        RequestBody body = RequestBody.create(encodedImage, mediaType);
+                        Request request = new Request.Builder()
+                                .url(postUrl)
+                                .post(body)
+                                .build();
 
-        @Override
-        public void run() {
-            super.run();
-            while (!interrupted()) {
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (this) {
-                    if (CaptureActivity.this.analyzing)
-                        continue;
-                    CaptureActivity.this.analyzing = true;
-                }
-                CaptureActivity.this.runOnUiThread(() -> {
-                    File picturePath = new File(CaptureActivity.this.getFilesDir(), "picture.jpeg");
-                    ImageCapture.OutputFileOptions outputFileOptions =
-                            new ImageCapture.OutputFileOptions.Builder(picturePath).build();
-                    imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(CaptureActivity.this),
-                            new ImageCapture.OnImageSavedCallback() {
-                                @Override
-                                public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                                    String encodedImage;
-                                    try {
-                                        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(picturePath));
-                                        byte[] bytes = new byte[inputStream.available()];
-                                        inputStream.read(bytes, 0, bytes.length);
-                                        inputStream.close();
-                                        encodedImage = Base64.encodeToString(bytes, Base64.DEFAULT);
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                        CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
-                                                R.string.capture_failed,
-                                                Toast.LENGTH_LONG).show());
-                                        return;
+                        client.newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                                e.printStackTrace();
+                                call.cancel();
+                                CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
+                                        R.string.network_failed,
+                                        Toast.LENGTH_LONG).show());
+                            }
+
+                            @Override
+                            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                                try {
+                                    if (response.code() != 200) {
+                                        throw new IOException("Request failed.");
                                     }
-                                    OkHttpClient client = new OkHttpClient.Builder()
-                                            .connectTimeout(10, TimeUnit.SECONDS)
-                                            .writeTimeout(10, TimeUnit.SECONDS)
-                                            .readTimeout(30, TimeUnit.SECONDS).build();
-                                    RequestBody body = RequestBody.create(encodedImage, mediaType);
-                                    Request request = new Request.Builder()
-                                            .url(postUrl)
-                                            .post(body)
-                                            .build();
-
-                                    client.newCall(request).enqueue(new Callback() {
-                                        @Override
-                                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                                            e.printStackTrace();
-                                            call.cancel();
-                                            CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
-                                                    R.string.network_failed,
-                                                    Toast.LENGTH_LONG).show());
-                                            synchronized (CaptureActivity.this) {
-                                                CaptureActivity.this.analyzing = false;
-                                            }
-                                        }
-
-                                        @Override
-                                        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                                            try {
-                                                if (response.code() != 200) {
-                                                    throw new IOException("Request failed.");
-                                                }
-                                                JSONObject jsonObject = new JSONObject(Objects.requireNonNull(response.body()).string());
-                                                int success = jsonObject.getInt("success");
-                                                if (success == 0) {
-                                                    throw new IllegalStateException("Failed to analyse ID Card.");
-                                                }
-                                                String number = jsonObject.getString("number");
-                                                String birthYear = jsonObject.getString("year");
-                                                String birthMonth = jsonObject.getString("month");
-                                                String birthDay = jsonObject.getString("date");
-                                                String name = jsonObject.getString("name");
-                                                String gender = jsonObject.getString("gender");
-                                                String nationality = jsonObject.getString("nationality");
-                                                String address = jsonObject.getString("address");
-                                                SQLiteDatabase database = openOrCreateDatabase("captured_results.db", MODE_PRIVATE, null);
-                                                database.execSQL("CREATE TABLE IF NOT EXISTS results(id_number VARCHAR, name VARCHAR, nationality VARCHAR, gender VARCHAR, birth_year VARCHAR, birth_month VARCHAR, birth_day VARCHAR, address VARCHAR);");
-                                                SQLiteStatement statement = database.compileStatement("INSERT INTO results VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
-                                                statement.bindString(1, number);
-                                                statement.bindString(2, name);
-                                                statement.bindString(3, nationality);
-                                                statement.bindString(4, gender);
-                                                statement.bindString(5, birthYear);
-                                                statement.bindString(6, birthMonth);
-                                                statement.bindString(7, birthDay);
-                                                statement.bindString(8, address);
-                                                statement.executeInsert();
-                                                database.close();
-                                                CaptureActivity.this.runOnUiThread(() -> {
-                                                    Toast.makeText(CaptureActivity.this,
-                                                            android.R.string.ok,
-                                                            Toast.LENGTH_LONG).show();
-                                                    CaptureActivity.this.finish();
-                                                });
-                                            } catch (JSONException | IOException e) {
-                                                e.printStackTrace();
-                                                CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
-                                                        R.string.network_failed,
-                                                        Toast.LENGTH_LONG).show());
-                                            } catch (IllegalStateException e) {
-                                                e.printStackTrace();
-                                            } finally {
-                                                synchronized (CaptureActivity.this) {
-                                                    CaptureActivity.this.analyzing = false;
-                                                }
-                                            }
-                                        }
+                                    JSONObject jsonObject = new JSONObject(Objects.requireNonNull(response.body()).string());
+                                    int success = jsonObject.getInt("success");
+                                    if (success == 0) {
+                                        throw new IllegalStateException("Failed to analyse ID Card.");
+                                    }
+                                    String number = jsonObject.getString("number");
+                                    String birthYear = jsonObject.getString("year");
+                                    String birthMonth = jsonObject.getString("month");
+                                    String birthDay = jsonObject.getString("date");
+                                    String name = jsonObject.getString("name");
+                                    String gender = jsonObject.getString("gender");
+                                    String nationality = jsonObject.getString("nationality");
+                                    String address = jsonObject.getString("address");
+                                    SQLiteDatabase database = openOrCreateDatabase("captured_results.db", MODE_PRIVATE, null);
+                                    database.execSQL("CREATE TABLE IF NOT EXISTS results(id_number VARCHAR, name VARCHAR, nationality VARCHAR, gender VARCHAR, birth_year VARCHAR, birth_month VARCHAR, birth_day VARCHAR, address VARCHAR);");
+                                    SQLiteStatement statement = database.compileStatement("INSERT INTO results VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
+                                    statement.bindString(1, number);
+                                    statement.bindString(2, name);
+                                    statement.bindString(3, nationality);
+                                    statement.bindString(4, gender);
+                                    statement.bindString(5, birthYear);
+                                    statement.bindString(6, birthMonth);
+                                    statement.bindString(7, birthDay);
+                                    statement.bindString(8, address);
+                                    statement.executeInsert();
+                                    database.close();
+                                    CaptureActivity.this.runOnUiThread(() -> {
+                                        Toast.makeText(CaptureActivity.this,
+                                                R.string.recognize_succeed,
+                                                Toast.LENGTH_LONG).show();
+                                        CaptureActivity.this.finish();
+                                    });
+                                } catch (JSONException | IOException | IllegalStateException e) {
+                                    e.printStackTrace();
+                                    CaptureActivity.this.runOnUiThread(() -> {
+                                        Toast.makeText(CaptureActivity.this,
+                                                R.string.recognize_failed,
+                                                Toast.LENGTH_LONG).show();
                                     });
                                 }
-
-                                @Override
-                                public void onError(@NonNull ImageCaptureException error) {
-                                    CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
-                                            R.string.capture_failed,
-                                            Toast.LENGTH_LONG).show());
-                                    synchronized (CaptureActivity.this) {
-                                        CaptureActivity.this.analyzing = false;
-                                    }
-                                }
                             }
-                    );
-                });
-            }
-        }
-    };
+                        });
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException error) {
+                        error.printStackTrace();
+                        CaptureActivity.this.runOnUiThread(() -> Toast.makeText(CaptureActivity.this,
+                                R.string.capture_failed,
+                                Toast.LENGTH_LONG).show());
+                    }
+                }
+        );
+    }
 }
